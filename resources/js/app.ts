fa3b0b23eecab8815eba427459bfac268e9cc234
p2squareb <p2squareb@gmail.com>
2024-10-09 20:59:28 +0900

    firtst commit

---
+++ resources/js/app.ts
@@ -0,0 +1,31 @@
+import '../css/app.css';
+import './bootstrap';
+
+import { createInertiaApp, Link, Head } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createApp, DefineComponent, h } from 'vue';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob<DefineComponent>('./Pages/**/*.vue'),
+        ),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .component('Link', Link)
+            .component('Head', Head)
+            .mount(el);
+    },
+    progress: {
+        color: '#ff2d20',
+        includeCSS: true,
+        showSpinner: false,
+    },
+});
